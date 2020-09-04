39d1ca22e6b5787d7dde9fc8a0c888d7800556e9
Random9-cyber <jcgamernerd@gmail.com>
2020-09-04 09:01:07 -0300

    PrimeiroCommit

---
+++ ZIP2/Site.js
@@ -0,0 +1,46 @@
+var AllContent = document.getElementById("AllContent");
+var Caixa = document.querySelector(".Caixa");
+
+document.addEventListener("scroll", () => {
+
+    const PositionY = window.pageYOffset
+
+    // console.log(window.pageYOffset)
+
+    // if (pagePositionY == 381.6000061035156)
+    //     Caixa.append("oi");
+    Caixa.innerHTML = parseInt(PositionY)
+
+    if (PositionY <= 400) {
+        Caixa.innerHTML = "Meu pai";
+    } else {
+        Caixa.innerHTML = "Minha mãe ";
+    }
+
+    if (PositionY >= 932) {
+        Caixa.innerHTML = "Meu irmão";
+    }
+
+    if (PositionY >= 1441) {
+        Caixa.innerHTML = "Eu";
+    }
+
+    // if (PositionY == 0) {
+    //     Caixa.innerHTML = "Uma BREVE biografia da minha familia:";
+    // }
+
+    // /    switch (window.pageYOffset) {
+    //         case 400:
+    //             Caixa.innerHTML = "Meu pai";
+    //             break;
+
+    //         case 424:
+    //             Caixa.innerHTML = "Minha mae";
+    //             break;
+
+    //         case 975:
+    //             Caixa.innerHTML = "Meu irmao";
+    //             break;
+    //     }/
+
+})
